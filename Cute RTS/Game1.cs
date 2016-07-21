4086e9e4264b478f8686f0d2ad87275afb1090dd
Hii Yong Lian <yonglian146@gmail.com>
2016-07-21 19:32:23 +0800

    Added rigidbody and pathfinding

--- Cute RTS/Game1.cs
+++ Cute RTS/Game1.cs
@@ -4,17 +4,30 @@ using Microsoft.Xna.Framework.Input;
 using Nez;
 using Nez.Sprites;
 using Nez.TextureAtlases;
+using Nez.Tiled;
 
 namespace Cute_RTS
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
+    public enum Animation
+    {
+        WalkUp
+    }
     public class Game1 : Core
     {
+        Scene.SceneResolutionPolicy policy;
 
-        public Game1() : base(width: 1280, height: 720, isFullScreen: false, enableEntitySystems: false)
-        { }
+        public Game1() : base(isFullScreen: false, enableEntitySystems: false)
+        {
+            policy = Scene.SceneResolutionPolicy.ShowAllPixelPerfect;
+            Scene.setDefaultDesignResolution(1280, 720, policy);
+            Window.AllowUserResizing = true;
+        }
+
+
+
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -35,12 +48,42 @@ namespace Cute_RTS
             entity.addComponent(new Sprite(moon));
             entity.transform.position = new Vector2(400, 400);
 
+
+            var tiledEntity = myScene.createEntity("tiled-map-entity");
+            var tiledmap = content.Load<TiledMap>("destructable-map");
+            tiledEntity.addComponent(new TiledMapComponent(tiledmap,"main"));
+
             var atlas = myScene.content.Load<TextureAtlas>("FirstAtlas");
             var anim = atlas.getSpriteAnimation("run");
+
+            var friction = 0.3f;
+            var elasticity = 0.4f;
+            var mass = 1f;
+            Vector2 velocity = new Vector2(150, 0);
+
+
+            var rigidbody = new ArcadeRigidbody()
+                .setMass(mass)
+                .setFriction(friction)
+                .setElasticity(elasticity)
+                .setVelocity(velocity);
+
+
             var entityTwo = myScene.createEntity("some-dude");
-            var sprite = new Sprite<int>(3, anim);
-            entity.addComponent(sprite);
-            sprite.play(3);
+
+            entityTwo.transform.position = new Vector2(50, 50);
+
+            var sprite = new Sprite<Animation>(Animation.WalkUp, anim);
+
+            entityTwo.addComponent(sprite);
+            entityTwo.addComponent(new SimpleMover());
+            entityTwo.addComponent(new Pathfinder(tiledmap));
+            entityTwo.addComponent(rigidbody);
+            entityTwo.addCollider(new CircleCollider());
+            sprite.play(Animation.WalkUp);
+
+
+
 
 
             // set the scene so Nez can take over
