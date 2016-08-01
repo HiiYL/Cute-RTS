0d1fec16386a15cfa34cb993f6270575ca45c421
Hii Yong Lian <yonglian146@gmail.com>
2016-08-01 21:28:19 +0800

    Lowered delay it takes to move randomly on collision

--- Cute RTS/PathMover.cs
+++ Cute RTS/PathMover.cs
@@ -121,7 +121,7 @@ namespace Cute_RTS
             if (res.collider != null)
             {
                 collisionPos = entity.transform.position;
-                Core.schedule(1.5f, timer =>
+                Core.schedule(0.2f, timer =>
                 { if (collisionPos == entity.transform.position)
                 {
                     target = target + new Point(Nez.Random.range(-2, 3), Nez.Random.range(-2, 3));
