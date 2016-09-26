c935999c5bd63f9d8e41de9972f5d2f143e9c75b
Lee Zhen Yong <bruceoutdoors@gmail.com>
2016-09-26 16:03:40 +0800

    refactor gamescene. Swap AI for utility AI (haven't implemented)

---
+++ Cute RTS/AI/PlayerAI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Cute_RTS.AI
+{
+    class PlayerAI : Player
+    {
+        public PlayerAI(Color color, string name, Player enemy) : base(color, name)
+        {
+            addComponent(new UtilityPlayerAI());
+        }
+
+        
+    }
+}
